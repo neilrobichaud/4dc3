6563ef90a80dfe7412348a4619d9956e8736684c
Neil Robichaud <neil.robichaud@hotmail.com>
2019-02-28 09:04:15 -0500

    todo fixed

--- bulletin/lib/bulletin.ex
+++ bulletin/lib/bulletin.ex
@@ -14,32 +14,39 @@ defmodule User do
   def subscribe(topic_name, user_name) do
     case :global.whereis_name(topic_name) do
       #if undefined
-      undefined -> 
+      undefined ->
         pid = spawn(TopicManager,:start,[topic_name])
         send(pid, {:subscribe, self()})
         #TODO: SAVE GLOBAL SUBSCRIPTIONS
         subscriptions = [pid]+subscriptions
-      pid ->  
+      pid ->
         send(pid,{:subscribe, self()})
         subscriptions = [pid]+subscriptions
     end
-
   end
 
 
   def unsubscribe(topic_name,user_name) do
+    #lookup pid of topicmanager of this topic
     managerpid = :global.whereis_name(topic_name)
+    #filter subscriptions to remove that pid from the subscriptions list
     subscriptions = Enum.reject(subscriptions, fn x -> x == managerpid end)
+    #tell the manager to remove this User from it's list
     send(managerpid,{:unsubscribe,self()})
   end
+
   def post(user_name, topic_name, content) do
-    
+    #lookup pid of topicmanager
+    managerpid = :global.whereis_name(topic_name)
+    #send the post to the topicmanager of this topic
+    send(managerpid,{:post,self(),content})
+
   end
+
   def fetch_news() do
     #enumerate through subscriptions list and send each pid a :fetch message
     Enum.each(subscriptions,fn x -> send(x,{:fetch_news,self()}))
   end
-
 end
 
 defmodule TopicManager do
@@ -61,9 +68,10 @@ defmodule TopicManager do
         Enum.each(content, fn x -> send(caller, x)
         run(subscribers,content)
       {:post, poster, post}-> 
+        #send post to all subscribers/..../
+        Enum.each(subscribers,fn x -> send(x,{:post,poster,post}))
         #add this message to the content list
         run(subscribers,[content|{:post,poster,post}])
-        #send post to all subscribers/..../
     end
   end
 
