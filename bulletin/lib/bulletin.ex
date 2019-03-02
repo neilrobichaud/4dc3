0efb9ed3db349a04f502499b8d99ca2dad055661
Neil Edgar Vivian Robichaud <robichne@mcmaster.ca>
2019-03-02 12:30:05 -0500

    fault tolerance

--- bulletin/lib/bulletin.ex
+++ bulletin/lib/bulletin.ex
@@ -1,8 +1,3 @@
-defmodule Bulletin do
-
-end
-
-
 defmodule User do
   def start(user_name) do
     :global.register_name(user_name,self())
@@ -41,32 +36,62 @@ defmodule User do
     receive do
       {:post,poster,post} -> IO.puts("#{poster},#{post}")
       fetch_news()
-    after 1000 -> :empty
+    after 1000 -> :empty 
     end
   end
 end
 
 defmodule TopicManager do
   def start(topic_name) do
-    :global.register_name(topic_name,self())
     #register pid of topic manager under topic_name
-    run([],[])
+    :global.register_name(topic_name,self())
+    #for each connected node, spawn a new topicmanager and store it's pid
+    secondary_pids = List.foldl(Node.list(), [], fn x, acc -> acc ++ [Node.spawn(x, TopicManager, :start_secondary, [[],Node.self(),length(acc),topic_name])] end)
+    IO.puts("secondary_pids= #{inspect(secondary_pids)}")
+    run([],secondary_pids)
   end
-
-  def run(subscribers,content) do
+  #wait is the starting state for secondary nodes
+  def start_secondary(subscribers,master_node,rank,topic_name) do
+        Node.monitor(master_node,true)
+        wait(subscribers,rank,topic_name)
+  end
+  def wait(subscribers,rank,topic_name) do
+    #IO.puts("these are my subs #{rank}: #{subscribers}")
     receive do
-      #add pid to subscribers list
+      {:nodedown,_node} -> 
+        #master died, check if rank 0
+        #if rank == 0 do
+            # I am the master now!
+            :global.re_register_name(topic_name,self())
+            #IO.puts("msg: #{msg}")
+            #this only works for a single failure...btw
+            run(subscribers,[])
+        #end
+      {:subscribe,user_name} -> wait(subscribers++[user_name], rank,topic_name)
+      {:unsubscribe,user_name} -> 
+        #get rid of this username
+        subscribers_new = Enum.reject(subscribers, fn x -> x == user_name end)
+        wait(subscribers_new,rank,topic_name)
+    end
+  end
+  #only the master node gets to call run
+  def run(subscribers,secondary_pids) do
+    receive do
+      #add pid to subscribers list and notify all secondary managers
       {:subscribe,user_name} -> 
-        run(subscribers++[user_name], content)
+        #send all the secondary managers the new subscriber
+        Enum.each(secondary_pids, fn x -> send(x,{:subscribe,user_name}) end)
+        run(subscribers++[user_name],secondary_pids)
       {:unsubscribe,user_name} ->
+        Enum.each(secondary_pids, fn x -> send(x,{:unsubscribe,user_name}) end)
         #remove pid from subscribers list
         subscribers_new = Enum.reject(subscribers, fn x -> x == user_name end)
-        run(subscribers_new, content)
-      {:post, poster, post}-> 
-        #send post to all subscribers/..../
-        Enum.each(subscribers,fn x -> send(:global.whereis_name(x),{:post,poster,post}) end)
-        #add this message to the content list
-        run(subscribers,[content|{:post,poster,post}])
+        run(subscribers_new,secondary_pids)
+      {:post, poster, post} -> 
+        #send post to all subscribers
+        #IO.puts("My subscribers are: #{subscribers}")
+        Enum.each(subscribers,fn x -> unless :global.whereis_name(x) == :undefined do send(:global.whereis_name(x),{:post,poster,post}) end end)
+        run(subscribers,secondary_pids)
     end
   end
 
